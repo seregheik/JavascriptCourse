6d65238fe1278608331e94f106988d50f20978e8
Osasere Ik <edenosas1@gmail.com>
2022-09-27 01:56:19 +0100

    basic guidelines

---
+++ 02Basics/01Variables.js
@@ -0,0 +1,26 @@
+var fullName = 'Osasere Ikponmwosa';
+
+//how to name variables
+
+// do not start a variable with uppercase letters
+// permitted models
+// fullName - camelcase
+// full-name
+// full_name
+
+var courseName = 'React JS Bootcamp'; // string
+
+var isLoggedIn = false; // boolean always small case
+
+var loggedCount = 34; // number
+
+console.log(loggedCount);// refrencing variable whose value should be displayed
+console.log('loggedCount'); // display as it is
+
+var paymentMode;
+
+console.log(paymentMode);
+
+paymentMode = 'Credit Card';
+
+console.log(paymentMode);

2b93c13f529504dea465ab3b63d75ee268f81ad6
Osasere Ik <edenosas1@gmail.com>
2022-10-02 21:53:23 +0100

    using switch

---
+++ 02Basics/08ConditionalRoles.js
@@ -0,0 +1,25 @@
+// Create an application with following roles :
+// admin - gets full access 
+// subadmin - gets access to create/delete courses
+// testprep - gets access to create/delete tests 
+// user - gets access to consume content
+
+var user = "admi";
+
+switch (user) {
+    case "testprep":
+        console.log('gets access to create/delete tests');
+        break;
+    case "user":
+        console.log('gets access to consume content');
+        break;
+    case "subadmin":
+        console.log('gets access to create/delete courses');
+        break;
+    case "admin":
+        console.log('gets full access');
+        break;
+    default:
+        console.log('Trial user');
+        break;
+}

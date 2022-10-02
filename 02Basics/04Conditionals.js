54547984acd8b0a3672078c9b4e5d472ff4daba4
Osasere Ik <edenosas1@gmail.com>
2022-10-02 04:52:09 +0100

    conditional statement

---
+++ 02Basics/04Conditionals.js
@@ -0,0 +1,15 @@
+var temperature;
+
+//TODO: go to google to get data
+
+temperature = 18;
+
+if (temperature < 20) {
+    console.log("it's very cold outside")
+};
+
+if (temperature < 30) {
+    console.log("it's moderate outside")
+} else {
+    console.log("it's really Hot outside")
+}

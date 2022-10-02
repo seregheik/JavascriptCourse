c94422ecf98397c319bb46b942520321adc2adb3
Osasere Ik <edenosas1@gmail.com>
2022-10-02 12:43:13 +0100

    time to resume

---
+++ 02Basics/06ConditionalLogin.js
@@ -0,0 +1,15 @@
+// Alllow user to access course if he is :
+// logged in from email
+// logged in from Google
+// logged in from Facebook
+
+
+var email = true;
+var facebook = false;
+var google = false;
+
+if (email || facebook || google) {
+
+    console.log('Success')
+
+}

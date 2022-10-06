fa1671bb3e948f4c8863acf3ff46286d62269f7f
Osasere Ik <edenosas1@gmail.com>
2022-10-06 13:32:33 +0100

    falsy values

---
+++ 02Basics/09TruthAndFalsy.js
@@ -0,0 +1,13 @@
+// TODO: falsy values
+//  undefined 
+//  null
+//  0
+//  ''
+//  NaN // (Not a Number)
+
+var user = '';
+
+console.log(user)
+    // if (user) {
+    //     console.log('condition is true');
+    // }

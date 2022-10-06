868da46a6d906b1c487f4314e91cd1040e3a28b0
Osasere Ik <edenosas1@gmail.com>
2022-10-06 14:16:00 +0100

    functions

--- 03Intermediate/01Function.js
+++ 03Intermediate/01Function.js
@@ -3,5 +3,13 @@ function sayHello(name) {
     console.log(`Hello there ${name}, How are you?`)
 };
 
-sayHello(`lukas`)
-sayHello()
+// sayHello(`lukas`)
+// sayHello()
+
+function miguor() {
+    return 'hello in urhobo'
+};
+
+var greetings = miguor();
+
+console.log(greetings)

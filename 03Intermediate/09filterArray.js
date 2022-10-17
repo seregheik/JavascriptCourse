211e267bc5241f7f23bd05763898a5f93d22062a
Osasere Ikponmwosa <edenosas1@gmail.com>
2022-10-17 12:19:24 -0700

    array filter

---
+++ 03Intermediate/09filterArray.js
@@ -0,0 +1,13 @@
+var testArray = [2, 4, 6, 3, 1, 5, 9, 8];
+
+console.log(testArray.fill('empty', 2, 6));
+
+// first parameter is value
+// second parameter is position and it is inclusive
+// third parameter is position and it is exclusocve
+
+const myNumber = [23, 24, 66, 18, 72, 51, 84, 32, 90];
+
+const result = myNumber.filter((num) => (num > 40));
+
+console.log(result);

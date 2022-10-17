d88611bdb49575ecb3321a68dca4907d9c729a79
Osasere Ikponmwosa <edenosas1@gmail.com>
2022-10-17 11:58:49 -0700

    callback funtions

--- 03Intermediate/08arrayPartTwo.js
+++ 03Intermediate/08arrayPartTwo.js
@@ -1,5 +1,12 @@
-function isEven(element) {
-    return element % 2 === 0;
-}
+// function isEven(element) {
+//     return element % 2 === 0;
+// }
 
-console.log(isEven(2))
+// console.log(isEven(2))
+
+var resultTrue = [2, 4, 6, 8].every((e) => (e % 2 === 0));
+var resultFalse = [2, 4, 7, 8].every((e) => (e % 2 === 0));
+
+
+console.log(resultTrue);
+console.log(resultFalse);

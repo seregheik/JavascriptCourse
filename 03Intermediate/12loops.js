6bb458cf8b8452d1b5688013ca9e9df1893a8c90
Osasere Ikponmwosa <edenosas1@gmail.com>
2022-10-18 12:41:21 -0700

    looping

---
+++ 03Intermediate/12loops.js
@@ -0,0 +1,27 @@
+// for (i = 0; i < 10; i++) {
+//     console.log(i)
+// };
+
+const naijaStates = new Array(
+    "Edo",
+    "Lagos",
+    "Ogun",
+    "Rivers",
+    2012,
+    "Delta",
+    "Sokoto",
+    "Kwara"
+);
+
+// for (let i = 0; i < naijaStates.length; i++) {
+//     if (typeof naijaStates[i] !== 'string') break;
+//     console.log(naijaStates[i]);
+// }
+
+
+let i = 0
+
+while (i < naijaStates.length) {
+    console.log(naijaStates[i])
+    i++
+}

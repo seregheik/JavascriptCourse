3cbab2868b24165cf3a0330025ad38498f0d47c8
Osasere Ik <edenosas1@gmail.com>
2022-10-27 16:27:24 +0100

    this again

---
+++ 03Intermediate/15thisAgain.js
@@ -0,0 +1,18 @@
+console.log(this);
+
+var user = {
+    firstName: 'Hitesh',
+    courseCount: 4,
+    getCourseCount: function () {
+        console.log('line 7', this);
+    }
+};
+
+// for ( n in user) {
+//     console.log(`this is the key: ${n} and these are the values ${user[n]}`)
+// }
+
+
+user.getCourseCount() 
+// 'this' refers to global or window object for regular function call
+//in this case this was a function in object

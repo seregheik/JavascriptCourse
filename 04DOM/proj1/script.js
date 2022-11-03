686dfb5881ee943d81307ef900c92ab1f5c00cbb
Osasere Ik <edenosas1@gmail.com>
2022-11-03 12:23:57 +0100

    manipulating dom

--- 04DOM/proj1/script.js
+++ 04DOM/proj1/script.js
@@ -1,9 +1,31 @@
 var counter = document.querySelector('.counter');
 var followers = document.querySelector('.followers');
+var background = document.querySelector('.brown')
 
+let changer= 1;
 let count = 1;
+let bgColor = 'yellow'
+let bgColord = 'brown'
+
 
 setInterval(() => {
-    count++;
+  if (changer < 1) {
+   changer += 0.5;
+   // counter.innerText = count;
+   background.className = bgColor
+  } else {
+   changer = 0;
+   background.className = bgColord
+  }
+}, 240);
+
+
+setInterval(() => {
+  if (count < 1000) {
+    count += 1;
     counter.innerText = count;
-}, 600);
+  } else {
+    count = 0
+  }
+  
+}, 60);
